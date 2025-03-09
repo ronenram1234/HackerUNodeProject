09d25fa2bc5f2e04ab40d951b6fb2f3a24302632
Ronen Ram <ronenram1234@gmail.com>
2025-03-09 09:54:39 +0200

    start

---
+++ index.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else {
+  dotenv.config({ path: '.env.development' });
+}
