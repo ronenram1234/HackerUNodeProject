3b369b25e1cab41d85ee79ad16dc35fc48fbf538
Ronen Ram <ronenram1234@gmail.com>
2025-03-16 22:02:37 +0200

    card routes update

--- routes/cards.js
+++ routes/cards.js
@@ -1,133 +1,163 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express");
+const router = express.Router();
 const User = require("../models/User");
 const Card = require("../models/Card");
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt");
 const Joi = require("Joi");
-
-
+const auth = require("../middlewares/auth");
 
 // create new card
-const newCardSchema = Joi.object({
-    title: Joi.string().min(2).max(255).required(),
-    subtitle: Joi.string().min(2).max(255).required(),
-    description: Joi.string().min(2).max(1024).required(),
+const CardSchema = Joi.object({
+  title: Joi.string().min(2).max(255).required(),
+  subtitle: Joi.string().min(2).max(255).required(),
+  description: Joi.string().min(2).max(1024).required(),
+
+  phone: Joi.string()
+    .pattern(/^[0-9\- ]{10,15}$/) // Allows numbers, spaces, and hyphens
+    .required(),
+
+  email: Joi.string().email().required(),
+
+  web: Joi.string().uri().optional(),
+
+  image: Joi.object({
+    url: Joi.string().uri().required().allow(""),
+    alt: Joi.string().min(2).max(255).required().allow(""),
+  }).required(),
+
+  address: Joi.object({
+    state: Joi.string().allow("").optional(),
+    country: Joi.string().min(2).max(255).required(),
+    city: Joi.string().min(2).max(255).required(),
+    street: Joi.string().min(2).max(255).required(),
+    houseNumber: Joi.number().required(),
+    zip: Joi.number().optional(),
+  }).required(),
+
+  // bizNumber: Joi.number().optional(),
+  likes: Joi.array().items(Joi.string()).optional(),
+  bizNumber: { type: Number, unique: true },
+
+  user_id: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+  createdAt: Joi.date().default(() => new Date()),
+});
+
+router.post("/", auth, async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).send("Card details are missing");
+    }
+
+    if (!req.payload.isBusiness)
+      return res.status(400).send("only Business user can create new card");
+    req.body.user_id = req.payload._id;
+
+    // check input validation
+    const { error } = await CardSchema.validateAsync(req.body);
+    if (error) return res.status(400).send("error schema");
+
+    // create Card
+    card = new Card(req.body);
+    const newCard = await card.save();
+
+    return res.status(200).send(newCard);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
+
+// get all cards
+router.get("/", async (req, res) => {
+  try {
+    const cards = await Card.find();
+    return res.status(200).send(cards);
+  } catch (err) {
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
+
+// get all my cards
+router.get("/my-cards", auth, async (req, res) => {
+  try {
+    const cards = await Card.find({ user_id: req.payload._id });
+    if (!cards) return res.status(400).send("No cards found");
+    return res.status(200).send(cards);
+  } catch (err) {
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const cards = await Card.findById(req.params.id);
+    if (!cards) return res.status(400).send("No cards found");
+    return res.status(200).send(cards);
+  } catch (err) {
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
+
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const card = await Card.findById(req.params.id);
+    if (!card) return res.status(400).send("No cards found");
+  
+    if (req.payload._id.toString() !== card.user_id.toString())
+      return res.status(400).send("Only card owner can change card data");
+
+    const { error } = CardSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const updateCard = await Card.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    return res.status(200).send(updateCard);
+  } catch (err) {
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
+
+router.patch("/:id",  auth, async (req, res) => {
+  try {
+    const card = await Card.findById(req.params.id);
+    if (!card) return res.status(400).send("No cards found");
+    card.likes.push(req.payload._id)
+
+    const updateCard = await Card.findByIdAndUpdate(req.params.id, card, {
+      new: true,
+    });
+
+
+    return res.status(200).send(updateCard);
+  } catch (err) {
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+
+
+    need to complete the logic for admin and card owner
+
     
-    phone: Joi.string()
-      .pattern(/^[0-9\- ]{10,15}$/) // Allows numbers, spaces, and hyphens
-      .required(),
-  
-    email: Joi.string().email().required(),
-  
-    web: Joi.string().uri().optional(),
-  
-    image: Joi.object({
-      url: Joi.string().uri().required().allow(""),
-      alt: Joi.string().min(2).max(255).required().allow(""),
-    }).required(),
-  
-    address: Joi.object({
-      state: Joi.string().allow("").optional(),
-      country: Joi.string().min(2).max(255).required(),
-      city: Joi.string().min(2).max(255).required(),
-      street: Joi.string().min(2).max(255).required(),
-      houseNumber: Joi.number().required(),
-      zip: Joi.number().optional(),
-    }).required(),
-  
-    // bizNumber: Joi.number().optional(),
-    likes: Joi.array().items(Joi.string()).optional(),     
-    user_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
-    createdAt: Joi.date().default(() => new Date()),
-
-  });
-
-router.post("/",async (req,res)=>{
-    try {
-        console.log(req.body);
-  
-      // check input validation
-      const { error } = await newCardSchema.validateAsync(req.body);
-      if (error) return res.status(400).send("error schema");
-  
-      // create Card
-      card = new Card(req.body);
-      const newCard = await card.save();
-  
-     return res.status(200).send(`post / sucessful ${newCard}`);
-
-    } catch (err) {
-
-      console.log(err.message)
-      res.status(400).send(`Invalide request - ${err.message}`);
+    const card = await Card.findById(req.params.id);
+    if (!card) return res.status(400).send("No cards found");
     
-    }})
 
-router.get("/",async (req,res)=>{
-try{
-
-    return res.status(200).send("get / sucessful")
-}
-catch (err){
-   res.status(400).send(`Invalide request - ${err.message}`)
-}
-
-})
-
-router.get("/my-cards",async (req,res)=>{
-try{
-
-    return res.status(200).send("get /my-cards sucessful")
-}
-catch (err){
-   res.status(400).send(`Invalide request - ${err.message}`)
-}
-
-})
-router.get("/:id",async (req,res)=>{
-try{
-
-    return res.status(200).send("get /:id sucessful")
-}
-catch (err){
-   res.status(400).send(`Invalide request - ${err.message}`)
-}
-
-})
+    const updateCard = await Card.findByIdAndDelete(req.params.id, {
+      new: true,
+    });
 
 
+    return res.status(200).send(updateCard);
+  } catch (err) {
+    res.status(400).send(`Invalide request - ${err.message}`);
+  }
+});
 
-    router.put("/:id",async (req,res)=>{
-        try{
-        
-            return res.status(200).send("put /:id sucessful")
-        }
-        catch (err){
-           res.status(400).send(`Invalide request - ${err.message}`)
-        }
-        
-        })
-
-        router.patch("/:id",async (req,res)=>{
-            try{
-            
-                return res.status(200).send("patch /:id sucessful")
-            }
-            catch (err){
-               res.status(400).send(`Invalide request - ${err.message}`)
-            }
-            
-            })
-
-            router.delete("/:id",async (req,res)=>{
-                try{
-                
-                    return res.status(200).send("delete /:id sucessful")
-                }
-                catch (err){
-                   res.status(400).send(`Invalide request - ${err.message}`)
-                }
-                
-                })
-
-module.exports=router
+module.exports = router;
